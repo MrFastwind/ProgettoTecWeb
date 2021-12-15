71deeacec16dfab0bd554e6c6773c5f89919f383
MrFastwind <fastwind.k@gmail.com>
2021-12-15 18:15:59 +0100

    Adds a structured database

--- lib/database/DatabaseUpdater.php
+++ lib/database/DatabaseUpdater.php
@@ -4,13 +4,25 @@ namespace database{
 
     class DatabaseUpdater
     {
-        public function __construct(private DatabaseHelper $dbh)
-        {}
-
+        public function __construct(private DatabaseHelper $dbh){
+        }
+        
+        /**
+         * updateProduct
+         *
+         * @param  Product $p
+         * @return bool false if not ended well
+         */
         public function updateProduct(Product $p):bool{
             return false;
         }
-
+        
+        /**
+         * createProduct
+         *
+         * @param  Product $p
+         * @return bool false if not ended well
+         */
         public function createProduct(Product $p):bool
         {
             return false;
