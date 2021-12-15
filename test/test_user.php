b3493ae88a45e436bc0bc6fb114dd3df6e82f050
MrFastwind <fastwind.k@gmail.com>
2021-12-15 18:15:57 +0100

    uses the database manager

--- test/test_user.php
+++ test/test_user.php
@@ -3,10 +3,13 @@ include ("../bootstrap.php");
 
 $testUser=(int)1;
 //var_dump($dbh->getUserById($testUser));
-$factory=new database\DatabaseObjectFactory($dbh);
-
+$factory=$dbm->getFactory();
 $user = $factory->getUser($testUser);
 
 var_dump($user);
+
+$productList = $factory->getProducts(0,5);
+
+var_dump($productList);
 
 ?>
