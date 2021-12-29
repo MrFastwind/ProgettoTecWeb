9aec0c871df50f2ec4508407c489ae13c687ce35
MrFastwind <fastwind.k@gmail.com>
2021-12-29 18:28:22 +0100

    Add unit test

--- test/test_user.php
+++ test/test_user.php
@@ -1,15 +1,65 @@
 <?php
-include ("../bootstrap.php");
 
-$testUser=(int)1;
-//var_dump($dbh->getUserById($testUser));
-$factory=$dbm->getFactory();
-$user = $factory->getUser($testUser);
+namespace test{
+    include ("../bootstrap.php");
 
-var_dump($user);
+    use database\DatabaseManager;
+    use database\User;
+    use shop\Shop;
 
-$productList = $factory->getProducts(0,5);
 
-var_dump($productList);
+    class UserTest{
 
+            private $user = "test_user";
+            private $password = "test_user_password";
+            private $email = "test@example.com";
+
+        function __construct(private Shop $shop, private DatabaseManager $dbm){
+        }
+
+        public function registerUser()
+        {
+            assert(is_a(
+                $this->shop->getUserManager()->registerClient($this->user,$this->password,$this->email),
+                "database\User"),
+                "Should be of type User"
+            );
+        }
+
+        public function loginUser()
+        {
+            assert(
+                is_a(
+                    $this->shop->getUserManager()->login($this->user,$this->password),
+                    "database\User"),
+                    "Should be of type User"
+                );
+            
+        }
+
+        public function runTests(){
+            foreach(["registerUser","loginUser"] as $it){
+                try{
+                    call_user_func([$this,$it]);
+                }finally{}
+            }
+        }
+
+        public function legacy(){
+            
+        $testUser=(int)1;
+        $factory=$this->dbm->getFactory();
+        $user = $factory->getUser($testUser);
+
+        var_dump($user);
+
+        $productList = $factory->getProducts(0,5);
+
+        var_dump($productList);
+        }
+
+    }
+    (new UserTest($shop,$dbm))->runTests();
+
+}
 ?>
