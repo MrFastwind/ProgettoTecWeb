ef445cb10444d55e9cbd211eb06c4c308f824795
MrFastwind <fastwind.k@gmail.com>
2022-01-17 19:44:30 +0100

    adds new user notification

--- lib/shop/NotificationFactory.php
+++ lib/shop/NotificationFactory.php
@@ -3,11 +3,18 @@ namespace shop{
 
     use database\DatabaseRequests;
     use database\exceptions\DatabaseException;
+    use database\User;
 
 class NotificationFactory{
 
 
         function __construct(private DatabaseRequests $dbr){}
+
+        public function notifyNewUser(User $user):bool{
+            $username = $user->Username;
+            return $this->dbr->createNotification("Benvenuto $username, e grazie per esserti registrato",$user->UserID);
+        }
+
 
         public function notifyOrderCreation(int $userid,int $orderId):bool{
             return $this->dbr->createNotification("L'ordine $orderId è stato creato",$userid);
