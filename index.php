da3d58ae6617f8a4fcab41165eeb61aecfcdf984
Kruso <sirfranis420@gmail.com>
2022-01-18 16:24:52 +0100

    Added random products to index

--- index.php
+++ index.php
@@ -3,6 +3,7 @@
 
     $templateParams["title"] = "Home";
     $templateParams["name"] = "home.php";
+    $templateParams["prodotti"] = $dbm->getRequests()->getRandomProducts($n=4);
 
     require("./template/base.php");
 ?>
