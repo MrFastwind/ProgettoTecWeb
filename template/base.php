50455b008d9728fba014579ad61fba2c4f8fedc8
Kruso <sirfranis420@gmail.com>
2022-01-19 13:30:03 +0100

    Added bootstrap CDN files

--- template/base.php
+++ template/base.php
@@ -5,10 +5,12 @@
 <html lang="it">
     <meta charset="UTF-8"/>
     <meta name="viewport" content="width=device-width, initial-scale=1.0"/>
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-EVSTQN3/azprG1Anm3QDgpJLIm9Nao0Yz1ztcQTwFspd3yD65VohhpuuCOmLASjC" crossorigin="anonymous">
     <link rel="stylesheet" href="../css/style.css"/>
     <title><?php echo $templateParams["title"];?></title>
 </html>
 <body>
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-MrcW6ZMFYlzcLA8Nl+NtUVF0sA7MsXsP1UyJoMp4YLEuNSfAP+JcXn/tWtIaxVXM" crossorigin="anonymous"></script>
     <header>
         <nav>
             <div>
@@ -17,7 +19,7 @@
                     <button type="submit" id="menuButton" name="menuButton"></button>
                 </form>
             </div>
-            <ul>
+            <ul class="unstyled">
                 <li>
                     <a href="catalogo.php">Catalogo</a>
                 </li>
@@ -40,7 +42,7 @@
             <!--TODO: add logo-->
         </div>
     </header>
-    <main>
+    <main class="grid">
         <?php if(isset($templateParams["name"])){
             require($templateParams["name"]);
         }
