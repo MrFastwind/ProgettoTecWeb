7e27a0db05287ebb2907af581e19b4bc5b1ca64c
Kruso <sirfranis420@gmail.com>
2022-01-20 18:59:53 +0100

    Added basic style for login and signup pages

--- template/login_form.php
+++ template/login_form.php
@@ -1,20 +1,20 @@
 <form action="#" method="POST">
-    <h2>Login</h2>
+    <h2 class="text-white">LOGIN</h2>
     <?php if(isset($templateParams["erroreLogin"])): ?>
             <p><?php echo $templateParams["erroreLogin"]; ?></p>
     <?php endif; ?>
-        <ul>
-            <li>
-                <input type="text" id="username" name="username" />
-            </li>
-            <li>
-                <input type="password" id="password" name="password" />
-            </li>
-            <li>
-                <input type="submit" name="submit" value="Accedi" />
-            </li>
-        </ul>
-        <footer>
-            <p>Non hai un account? <a href="login_form.php">Registrati</a></p>
-        </footer>
+    <ul class="list-unstyled">
+        <li>
+            <input type="text" id="username" name="username" placeholder="Username / Email" />
+        </li>
+        <li>
+            <input type="password" id="password" name="password" placeholder="Password" />
+        </li>
+        <li>
+            <a href="login.php"> Accedi </a>
+        </li>
+    </ul>
+    <footer>
+        <p>Non hai un account? <a href="signup.php" class="text-warning">Registrati</a></p>
+    </footer>
 </form>
--- template/signup_form.php
+++ template/signup_form.php
@@ -1,20 +1,20 @@
 <form action="#" method="POST">
-    <h2>Sign up</h2>
-        <ul>
+    <h2 class="text-white">SIGN UP</h2>
+        <ul class="list-unstyled">
             <li>
-                <input type="text" id="username" name="username"/>
+                <input type="text" id="username" name="username" placeholder="Username"/>
             </li>
             <li>
-                <input type="text" id="email" name="email"/>
+                <input type="text" id="email" name="email" placeholder="email"/>
             </li>
             <li>
-                <input type="password" id="password" name="password"/>
+                <input type="password" id="password" name="password" placeholder="Password"/>
             </li>
             <li>
-                <input type="submit" name="submit" value="Registrati"/>
+                <a href="signup.php">Registrati</a>
             </li>
         </ul>
         <footer>
-            <p>Hai già un account? <a href="login_form.php">Accedi</a></p>
+            <p>Hai già un account? <a href="login_form.php" class="text-warning">Accedi</a></p>
         </footer>
 </form>
