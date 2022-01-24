08ae6ccebba13b9454d109d7f263fa333af32ae9
MrFastwind <fastwind.k@gmail.com>
2022-01-24 17:33:57 +0100

    Adds orders api endpoint

--- api/order.php
+++ api/order.php
@@ -1,22 +1,51 @@
 <?php
 require_once($_SERVER["DOCUMENT_ROOT"] . "/bootstrap.php");
 use api\response;
+use database\OrderStatus;
+
 header("Access-Control-Allow-Origin: *");
 header("Content-Type: application/json; charset=UTF-8");
 
-if (!$shop->getUserManager()->isUserLogged()){
-    Response::error("Bad Login");
-}
+
 
 if(!key_exists('id',$_GET)){
-    Response::error("no order id!");
+    echo Response::error("no order id!");
+    return;
 }
+$id = $_GET['id'];
+$logged=$shop->getUserManager()->isUserLogged();
+
 
 if(key_exists('status',$_GET)){
-    //Set Status
-    $shop->getOrderManager()->updateOrderStatus($id,$status);
+    if(!$logged){
+        http_response_code(401);
+        echo Response::error("not logged");
+        return;
+    }
+    if(!$_SESSION["User"]->isVendor){
+        http_response_code(403);
+        echo Response::error("no permission");
+    }
+
+    try{
+    $orderStatus = OrderStatus::getStatusByString($_GET["status"]);
+    }catch(ValueError $e){
+        echo Response::error($e->getMessage());
+        return;
+    }
+    if($shop->getOrderManager()->updateOrderStatus($id,$orderStatus)){
+        echo Response::ok();
+    }else{
+        http_response_code(503);
+        echo Response::error("Internal Error");
+    }
 }else{
-    //get Status
+    try{
+        $order=$dbm->getFactory()->getOrder($id);
+        echo Response::ok(data: ["status"=>$order->OrderStatusID]);
+    }catch(Exception $e){
+        echo Response::error("No order with that id");
+    }
 }
 
 ?>
