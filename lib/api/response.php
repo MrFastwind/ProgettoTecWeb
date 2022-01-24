29baa00fe968ce742ade0758aff0d31938ef38d1
MrFastwind <fastwind.k@gmail.com>
2022-01-24 16:24:31 +0100

    Makes json pretty

--- lib/api/response.php
+++ lib/api/response.php
@@ -5,11 +5,11 @@ namespace api{
         const ERROR_NO_ELEMENT_FOR_ID = "No element with that id";
 
         public static function error(string $value){
-            return json_encode(array("status"=>"ERROR","value"=>$value));
+            return json_encode(array("status"=>"ERROR","value"=>$value),JSON_PRETTY_PRINT);
         }
         
         public static function ok(mixed $value){
-            return json_encode(array("status"=>"OK","value"=>$value));
+            return json_encode(array("status"=>"OK","value"=>$value),JSON_PRETTY_PRINT);
         }
         
     }
