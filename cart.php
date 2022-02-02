94dc0835c5b84ea181c0300bc5db54cd74d90b55
Kruso <sirfranis420@gmail.com>
2022-02-02 13:51:15 +0100

    Added basic cart page

--- cart.php
+++ cart.php
@@ -1,3 +1,13 @@
 <?php
+    require_once("./bootstrap.php");
 
+    if(isset($_SESSION["User"])){
+        $user = $_SESSION["User"];
+        $templateParams["cart"] = $dbm->getFactory()->getUserCart($user->UserID);
+    }
+    else{
+        $templateParams["name"] = "login.php";
+    }
+
+    require("./template/base.php");
 ?>
