a09d347c44527cbf99d4ec3ad9074d07e97506e3
Kruso <sirfranis420@gmail.com>
2022-02-03 18:15:27 +0100

    Added paramaters for template file

--- cart.php
+++ cart.php
@@ -4,9 +4,11 @@
     if(isset($_SESSION["User"])){
         $user = $_SESSION["User"];
         $templateParams["cart"] = $dbm->getFactory()->getUserCart($user->UserID);
+        $templateParams["name"] = "cart_section.php";
+        $templateParams["title"] = "Carrello";
     }
     else{
-        $templateParams["name"] = "login.php";
+        header("location: /login.php");
     }
 
     require("./template/base.php");
