772738abda3f468ba7f2aa6b51224c0070221565
Kruso <sirfranis420@gmail.com>
2022-02-04 18:02:41 +0100

    Fixed img path

--- venditore.php
+++ venditore.php
@@ -1,9 +1,12 @@
 <?php
+
     require_once("./bootstrap.php");
 
     if(isset($_POST["productName"]) && isset($_POST["productDescription"]) && isset($_POST["quantity"]) && isset($_POST["price"]) && isset($_POST["category"])){
+        $imageName = str_replace("/","",$_POST["productName"]);
         $user = $_SESSION["User"];
-        $dbm->getRequests()->createProduct($_POST["productName"], $_POST["productDescription"], "IMG_DIR/{$_POST["productName"]}.png", $_POST["quantity"], $_POST["price"], $user->UserID, $_POST["category"]);
+        $categoryId = array_search($_POST["category"],$dbm->getFactory()->getCategories());
+        $dbm->getRequests()->createProduct($_POST["productName"], $_POST["productDescription"], "$imageName.png", $_POST["quantity"], $_POST["price"], $user->UserID, $categoryId);
         $templateParams["success"] = "Prodotto aggiunto";
     }
 
