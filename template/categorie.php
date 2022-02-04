3fd69a9fd26357ef39dc70bba5bbf65386782fb8
Kruso <sirfranis420@gmail.com>
2022-02-04 11:39:42 +0100

    Fixed alignment of items and added style

--- template/categorie.php
+++ template/categorie.php
@@ -1,10 +1,12 @@
 <header>
     <h2 class="text-white">CATEGORIE</h2>
 </header>
-<ul class="categories">
+<ul class="categories list-unstyled">
     <?php foreach($templateParams["categorie"] as $key=>$name): ?>
+    <li>
         <?php
         echo('<a href="categoria.php?id='.$key.'">'.$name. '</a>');
-        /*echo $categoria; */?>
+        ?>
     <?php endforeach ?>
+    </li>
 </ul>
