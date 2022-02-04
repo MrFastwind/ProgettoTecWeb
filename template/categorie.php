84e1358eab4081b563e7b748422f714ab6e85cd0
Kruso <sirfranis420@gmail.com>
2022-02-04 18:05:58 +0100

    Put tags inside a section

--- template/categorie.php
+++ template/categorie.php
@@ -1,12 +1,14 @@
 <header>
     <h2 class="text-white">CATEGORIE</h2>
 </header>
-<ul class="categories list-unstyled">
-    <?php foreach($templateParams["categorie"] as $key=>$name): ?>
-    <li>
-        <?php
-        echo('<a href="categoria.php?id='.$key.'">'.$name. '</a>');
-        ?>
-    <?php endforeach ?>
-    </li>
-</ul>
+<section>
+    <ul class="categories list-unstyled">
+        <?php foreach($templateParams["categorie"] as $key=>$name): ?>
+        <li>
+            <?php
+            echo('<a href="categoria.php?id='.$key.'">'.$name. '</a>');
+            ?>
+        <?php endforeach ?>
+        </li>
+    </ul>
+</section>
