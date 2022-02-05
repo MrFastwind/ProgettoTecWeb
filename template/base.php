18f1896a0ba97f669698a81c99e69c4ecdef0027
Kruso <sirfranis420@gmail.com>
2022-02-05 15:56:59 +0100

    Added basic menu

--- template/base.php
+++ template/base.php
@@ -7,18 +7,20 @@
     <link href="https://fonts.googleapis.com/css2?family=Raleway:wght@400;700&display=swap" rel="stylesheet"/> 
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-EVSTQN3/azprG1Anm3QDgpJLIm9Nao0Yz1ztcQTwFspd3yD65VohhpuuCOmLASjC" crossorigin="anonymous">
     <link rel="stylesheet" href="/css/style.css"/>
+    <script type="text/javascript" src="/js/nav.js"></script>
     <title><?php echo $templateParams["title"];?></title>
 </html>
 <body>
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-MrcW6ZMFYlzcLA8Nl+NtUVF0sA7MsXsP1UyJoMp4YLEuNSfAP+JcXn/tWtIaxVXM" crossorigin="anonymous"></script>
     <header>
-        <nav>
-        <form action="search.php" method="POST">
-            <input type="text" id="search" name="search"/>
-            <button type="submit" class="searchbtn">
-                <img src="/img/search.png" alt="Search">
-            </button>
-        </form>
+        <nav id="myNav" class="overlay">
+            <a href="javascript:void(0)" class="closebtn text-decoration-none text-dark" onclick="closeNav()">&times;</a>
+            <form action="search.php" method="POST">
+                <input type="text" id="search" name="search"/>
+                <button type="submit" class="searchbtn">
+                    <img src="/img/search.png" alt="Search">
+                </button>
+            </form>
             <ul class="list-unstyled">
                 <li>
                     <a class="text-decoration-none text-dark" href="catalogo.php">Catalogo</a>
@@ -55,6 +57,7 @@
         <div>
             <!--TODO: add logo-->
         </div>
+        <span style="font-size: 30px;cursor:pointer" onclick="openNav()">&#9776;</span>
     </header>
     <main class="flex-container">
         <?php if(isset($templateParams["name"])){
