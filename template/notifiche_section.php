f744ac23e1b491dd8e226a0ba8dfe74fe9e491d2
Kruso <sirfranis420@gmail.com>
2022-02-07 21:43:39 +0100

    Fixed width and table positioning

--- template/notifiche_section.php
+++ template/notifiche_section.php
@@ -1,17 +1,19 @@
 <section>
-    <header>
-        <h2 class="text-white">NOTIFICHE</h2>
-    </header>
-    <table class="notifications">
-        <tr>
-            <th>Data</th>
-            <th>Notifica</th>
-        </tr>
-        <?php foreach($notifications as $notification): ?>
+    <div class="tableContainer">
+        <header>
+            <h2 class="text-white">NOTIFICHE</h2>
+        </header>
+        <table class="notifications">
             <tr>
-                <td><?php echo($notification["Time"]);?></td>
-                <td><?php echo($notification["Text"]);?></td>
+                <th>Data</th>
+                <th>Notifica</th>
             </tr>
-        <?php endforeach ?>
-    </table>
+            <?php foreach($notifications as $notification): ?>
+                <tr>
+                    <td><?php echo($notification["Time"]);?></td>
+                    <td><?php echo($notification["Text"]);?></td>
+                </tr>
+            <?php endforeach ?>
+        </table>
+    </div>
 </section>
