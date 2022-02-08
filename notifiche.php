59b22c8210ad02d24c96dc59563fa0941e476b88
Kruso <sirfranis420@gmail.com>
2022-02-08 16:45:36 +0100

    Reversed notifications order

--- notifiche.php
+++ notifiche.php
@@ -3,6 +3,7 @@
 
     $user = $_SESSION["User"];
     $notifications = $dbm->getRequests()->getUserNotifications($user->UserID);
+    $notifications = array_reverse($notifications);
 
     $templateParams["name"] = "notifiche_section.php";
     $templateParams["title"] = "Notifiche";
