0c11c482e902abec882bcec77330c5c7d18b81f9
Kruso <sirfranis420@gmail.com>
2022-02-09 10:45:58 +0100

    Added check for out of stock products

--- template/products_section.php
+++ template/products_section.php
@@ -8,41 +8,43 @@
     <?php if(isset($products)): ?>
         <ul class="list-unstyled accordion" id="accordion">
         <?php foreach ($products as $product): ?>
-            <li>
-                <div class="accordion-item">
-                    <h2 class="accordionHeader" id="<?php echo($product->Name);?>Label">
-                        <button class="accordion-button collapsed" type="button" data-bs-toggle="collapse" data-bs-target="#<?php echo($product->Name);?>" aria-expanded="false" aria-controls="<?php echo($product->Name);?>"><?php echo($product->Name);?></button>
-                    </h2>
-                    <div id="<?php echo($product->Name);?>" class="accordion-collapse collapse" aria-labelledby="<?php echo($product->Name);?>Label" data-bs-parent="#accordion" style="background-image: linear-gradient(rgba(220,220,220,.6), rgba(220,220,220,.6)), url('<?php echo(retrieveImage($product->Image,IMG_DIR));?>');">
-                        <div class="accordion-body">
-                            <ul class="list-unstyled">
-                                <li>
-                                    <p class="text-black"><?php echo($product->Description);?></p>
-                                </li>
-                                <li>
-                                    <p class="text-black">Prezzo per singola unità: <?php echo($product->Price);?>.00€</p>
-                                </li>
-                            </ul>
-                            <form action="addTocart.php" method="POST">
-                                <ul class="list-unstyled addToCart">
+            <?php if($product->Quantity!=0): ?>
+                <li>
+                    <div class="accordion-item">
+                        <h2 class="accordionHeader" id="<?php echo($product->Name);?>Label">
+                            <button class="accordion-button collapsed" type="button" data-bs-toggle="collapse" data-bs-target="#<?php echo($product->Name);?>" aria-expanded="false" aria-controls="<?php echo($product->Name);?>"><?php echo($product->Name);?></button>
+                        </h2>
+                        <div id="<?php echo($product->Name);?>" class="accordion-collapse collapse" aria-labelledby="<?php echo($product->Name);?>Label" data-bs-parent="#accordion" style="background-image: linear-gradient(rgba(220,220,220,.6), rgba(220,220,220,.6)), url('<?php echo(retrieveImage($product->Image,IMG_DIR));?>');">
+                            <div class="accordion-body">
+                                <ul class="list-unstyled">
                                     <li>
-                                        <select name="quantity">
-                                            <?php for($i=1; $i<=$product->Quantity; $i++): ?>
-                                                <option value="<?php echo($i);?>"><?php echo($i);?></option>
-                                            <?php endfor ?>
-                                        </select>
+                                        <p class="text-black"><?php echo($product->Description);?></p>
                                     </li>
                                     <li>
-                                        <input type="submit" name="add" value="🛒Aggiungi al carrello" id="addToCart"/>
-                                    </li>
-                                    <li>
-                                        <input type="hidden" name="product" value="<?php echo($product->ProductID);?>"/>
+                                        <p class="text-black">Prezzo per singola unità: <?php echo($product->Price);?>.00€</p>
                                     </li>
                                 </ul>
-                            </form>
+                                <form action="addTocart.php" method="POST">
+                                    <ul class="list-unstyled addToCart">
+                                        <li>
+                                            <select name="quantity">
+                                                <?php for($i=1; $i<=$product->Quantity; $i++): ?>
+                                                    <option value="<?php echo($i);?>"><?php echo($i);?></option>
+                                                <?php endfor ?>
+                                            </select>
+                                        </li>
+                                        <li>
+                                            <input type="submit" name="add" value="🛒Aggiungi al carrello" id="addToCart"/>
+                                        </li>
+                                        <li>
+                                            <input type="hidden" name="product" value="<?php echo($product->ProductID);?>"/>
+                                        </li>
+                                    </ul>
+                                </form>
+                            </div>
                         </div>
                     </div>
-                </div>
+            <?php endif ?>
         <?php endforeach ?>
     </ul>
     <?php else: ?>
